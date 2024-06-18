574ed4559ec5c2c37e650a28c29610f9e604ce0f
STMT017 <sxy2367221250@163.com>
2024-06-18 16:00:14 +0800

    提供一种修复ccls根路径问题
    解决方案:
    https://vi.stackexchange.com/questions/43426/ccls-configuration-in-nvim-lspconfig
    简单来说就是在你的项目根路径创建一个空的.ccls文件即可。

--- lua/plugins/core/lspconfig.lua
+++ lua/plugins/core/lspconfig.lua
@@ -86,6 +86,15 @@ return {
       capabilities = capabilities,
       filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
       offset_encoding = "utf-32",
+      init_options = {
+        compilationDatabaseDirectory = "build",
+        index = {
+          threads = 0,
+        },
+        clang = {
+          excludeArgs = { "-frounding-math" },
+        },
+      },
     }
 
     -- gopls
