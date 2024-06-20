8139c5ff5d916df85008c37dc7e63d45f45d0b89
STMT017 <sxy2367221250@163.com>
2024-06-20 19:58:00 +0800

    更新lsp相关配置

--- lua/plugins/core/lspconfig.lua
+++ lua/plugins/core/lspconfig.lua
@@ -13,19 +13,22 @@ return {
     local util = require "lspconfig/util"
 
     local servers = {
-      "html",
-      "cssls",
-      -- "clangd",
-      "eslint",
+      "html", -- mason-lspconfig
+      "cssls", -- mason-lspconfig
+      -- "eslint",
       -- "pyright",
-      "ruff_lsp",
+      -- "ruff_lsp",
       -- "tsserver",
       -- "gopls",
       "svelte",
       -- "lua_ls",
+      "ccls", -- usr
       "graphql",
       "emmet_ls",
-      --"tailwindcss",  --uninstalled
+      "pylsp", -- mason-lspconfig
+      "rust_analyzer", -- mason
+      "java_language_server", -- mason-lspconfig
+      "tailwindcss", -- mason
     }
 
     -- 代码诊断图标
@@ -87,6 +90,7 @@ return {
         "configure.ac",
         ".git"
       ),
+      single_file_support = false,
     }
 
     -- ccls
@@ -127,7 +131,30 @@ return {
         },
       },
     }
-  end,
+    -- emmet_ls
+    lspconfig.emmet_ls.setup {
+      capabilities = capabilities,
+      filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+    }
 
-  --
+    -- graphql
+    lspconfig.graphql.setup {
+      capabilities = capabilities,
+      filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+    }
+
+    -- svelte
+    lspconfig.svelte.setup {
+      capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePost", {
+          pattern = { "*.js", "*.ts" },
+          callback = function(ctx)
+            -- Here use ctx.match instead of ctx.file
+            client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+          end,
+        })
+      end,
+    }
+  end,
 }
